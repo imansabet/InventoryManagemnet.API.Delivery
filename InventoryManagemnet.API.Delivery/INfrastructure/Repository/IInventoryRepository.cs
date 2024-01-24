bef5f3f13f954050d52a2683b46fc7c1653acf8b
iman <dev.imansabet@gmail.com>
2024-01-24 22:05:57 +0330

    Create Repository interface : IInventoryRepository

---
+++ InventoryManagemnet.API.Delivery/INfrastructure/Repository/IInventoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using InventoryManagemnet.API.Delivery.DTO.InternalAPIModel.Request;
+using InventoryManagemnet.API.Delivery.DTO.InternalAPIModel.Response;
+
+namespace InventoryManagemnet.API.Delivery.INfrastructure.Repository
+{
+    public interface IInventoryRepository
+    {
+        public interface IInventoryRepository
+        {
+            Task<List<InventoryServiceResponse>> GetAllInventoryItemsAsync();
+            Task<InventoryServiceResponse> GetInventoryItemAsync(Guid productId);
+            Task AddInventoryItemAsync(InventoryServiceRequest inventoryItem);
+            Task UpdateInventoryItemAsync(Guid productId, InventoryServiceRequest updatedInventoryItem);
+            Task DeleteInventoryItemAsync(Guid productId);
+        }
+    }
+}
