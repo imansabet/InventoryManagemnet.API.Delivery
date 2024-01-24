f54d5ea2048927c19d87ea4e8a7f141643b727ed
iman <dev.imansabet@gmail.com>
2024-01-24 21:22:38 +0330

    Add InventoryItem Entity

---
+++ InventoryManagemnet.API.Delivery/INfrastructure/Entities/InventoryItem.cs
@@ -0,0 +1,9 @@
+﻿namespace InventoryManagemnet.API.Delivery.INfrastructure.Entities
+{
+    public class InventoryItem
+    {
+        public Guid ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+    }
+}
